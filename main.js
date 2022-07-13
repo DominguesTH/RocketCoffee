ffa97794e274697952463bc3733eed13c43ac771
Thiago Domingues <ts.domingues95@gmail.com>
2022-07-13 03:44:11 -0300

    Versão mobile finalizada

--- main.js
+++ main.js
@@ -20,15 +20,62 @@ for(const link of links) {
 }
 
 /* scroll header stylle */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function() {
   if(window.scrollY >= navHeight){
     header.classList.add('scroll')
   }
   else {
     header.classList.remove('scroll')
   }
+}
+
+
+/* Place carousel swiper */
+const swiper = new Swiper('.swiper', {
+ slidesPerView: 1,
+ pagination: {
+  el:'.swiper-pagination'
+ },
+ mousewheel: true,
+ keyboard: true
 })
 
+/* ScrolReveal*/
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+` #home h1, #home .button, #home .image, 
+#coffees .title, #coffees .card, 
+#place .swiper,
+#about .title, #about .text,
+#contact .title, #contact .button, #contact .links,
+footer .copyright, footer .social
+`,{ interval: 100})
+
+/* Back to top*/
+
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if(window.scrollY >= 560){
+    backToTopButton.classList.add('show')
+  } else {
+   backToTopButton.classList.remove('show')
+}
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function() {
+  changeHeaderWhenScroll()
+  backToTop()
+ })
